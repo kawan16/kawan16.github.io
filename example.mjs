bdabfb52f7640293fed3564a6c5617832858ea13
kawan16 <karl.devooght@gmail.com>
2020-09-19 16:51:57 +0200

    Change shape

--- example.mjs
+++ example.mjs
@@ -42,8 +42,20 @@ function init() {
 
     //
 
-    var geometry = new THREE.CylinderBufferGeometry( 0.1, 0.1, 0.2, 32 ).translate( 0, 0.1, 0 );
+    var heartShape = new THREE.Shape();
 
+    heartShape.moveTo( 25, 25 );
+    heartShape.bezierCurveTo( 25, 25, 20, 0, 0, 0 );
+    heartShape.bezierCurveTo( 30, 0, 30, 35,30,35 );
+    heartShape.bezierCurveTo( 30, 55, 10, 77, 25, 95 );
+    heartShape.bezierCurveTo( 60, 77, 80, 55, 80, 35 );
+    heartShape.bezierCurveTo( 80, 35, 80, 0, 50, 0 );
+    heartShape.bezierCurveTo( 35, 0, 25, 25, 25, 25 );
+
+    var extrudeSettings = { amount: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
+
+    var geometry = new THREE.ExtrudeBufferGeometry( heartShape, extrudeSettings );
+    
     function onSelect() {
 
         if ( reticle.visible ) {
